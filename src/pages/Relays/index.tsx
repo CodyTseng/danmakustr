bdfb0a5183d5e3d09bcbd1dac77e0dd55e370bc5
codytseng <codytseng@outlook.com>
2024-09-05 16:32:18 +0800

    fix: relay url truncate

--- src/pages/Relays/index.tsx
+++ src/pages/Relays/index.tsx
@@ -95,7 +95,7 @@ function Relay({ relay, remove }: { relay: TRelay; remove: (url: string) => void
   const url = new URL(relayUrl)
   return (
     <div className="flex justify-between items-center hover:text-primary">
-      <div className="flex items-center gap-2">
+      <div className="flex flex-1 w-0 items-center gap-2">
         <Avatar
           className={
             'w-6 h-6 border-2 border-solid ' +
@@ -105,7 +105,7 @@ function Relay({ relay, remove }: { relay: TRelay; remove: (url: string) => void
           <AvatarImage src={getFaviconUrl(url)} />
           <AvatarFallback>{url.host.slice(0, 2).toUpperCase()}</AvatarFallback>
         </Avatar>
-        <div>{relayUrl}</div>
+        <div className="truncate">{relayUrl}</div>
       </div>
       <Button className="w-6 h-6 p-0" variant="ghost" onClick={() => remove(relayUrl)}>
         <Trash2 size={14} className="text-destructive" />
